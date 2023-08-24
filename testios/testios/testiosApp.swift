a306d763f514c57d235a46c434f88449015bbd40
Grant Fortune <grfortun@amazon.com>
2023-08-24 18:31:11 +0200

    initial

---
+++ testios/testios/testiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  testiosApp.swift
+//  testios
+//
+//  Created by Fortune, Grant on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct testiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
